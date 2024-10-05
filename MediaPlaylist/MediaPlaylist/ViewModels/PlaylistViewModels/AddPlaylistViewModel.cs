bbfc9c0c101cf3c3933428b4d6be800fee145b6a
Ranim <ranim_hanna@hotmail.com>
2024-10-05 23:12:05 +0200

    Add status message property

--- MediaPlaylist/MediaPlaylist/ViewModels/PlaylistViewModels/AddPlaylistViewModel.cs
+++ MediaPlaylist/MediaPlaylist/ViewModels/PlaylistViewModels/AddPlaylistViewModel.cs
@@ -1,21 +1,33 @@
-﻿using MediaPlaylist.Core;
-using MediaPlaylist.Services;
-using MediaPlaylistBL;
-using System;
+﻿using System;
 using System.Diagnostics;
 using System.Windows;
-using System.Windows.Input;
-
+using System.Windows.Threading;
+using MediaPlaylist.Core;
+using MediaPlaylist.Services;
+using MediaPlaylistBL;
 
 namespace MediaPlaylist.ViewModels.PlaylistViewModels
 {
     public class AddPlaylistViewModel : ViewModelBase
     {
-        private readonly INavigationService _navigationService;
-        private PlaylistManager _playlistManager;
-        private NavigationBarViewModel _navigationBarViewModel;
+        private readonly ApplicationManager _appManager;
+        private readonly NavigationBarViewModel _navigationBarViewModel;
 
         #region Properties
+        public string _statusMessage = string.Empty;
+        public string StatusMessage
+        {
+            get => _statusMessage;
+            set 
+            {
+                if (_statusMessage != value)
+                {
+                    _statusMessage = value;
+                    OnPropertyChanged(nameof(StatusMessage));
+                }
+            }
+        }
+
         private string _title = string.Empty;
         public string Title
         {
@@ -39,34 +51,50 @@ namespace MediaPlaylist.ViewModels.PlaylistViewModels
         }
         #endregion
 
-        public ICommand AddPlaylistCommand { get; }
+        public AsyncCommandBase AddPlaylistCommand { get; }
 
-        public AddPlaylistViewModel(
-            INavigationService navigationService, PlaylistManager playlistManager, NavigationBarViewModel navBarViewModel)
+        public AddPlaylistViewModel(INavigationService navigationService, ApplicationManager manager, NavigationBarViewModel navBarViewModel)
         {
-            _navigationService = navigationService;
-            _playlistManager = playlistManager;
+            _appManager = manager;
             _navigationBarViewModel = navBarViewModel;
-
-            AddPlaylistCommand = new CommandBase(_ => AddPlaylist());
+            AddPlaylistCommand = new AsyncCommandBase(async _ => await AddPlaylist());
         }
 
-
-        public void AddPlaylist()
+        public async Task AddPlaylist()
         {
             try
             {
                 if (string.IsNullOrEmpty(Title)) throw new Exception("Title cannot be empty");
 
-                _playlistManager.CreatePlaylist(Title, Description);
-                PlaylistViewModel viewModel = new PlaylistViewModel(_playlistManager.GetLastPlaylist());
+                await _appManager.AddPlaylist(Title, Description);
+                PlaylistViewModel viewModel = new PlaylistViewModel(await _appManager.GetLastPlaylist());
                 _navigationBarViewModel.Playlists.Add(viewModel);
+                ShowMessage();
+
+                // Clear inputs
+                Title = string.Empty;
+                Description = string.Empty;
             }
             catch (Exception ex)
             {
                 //Debug.WriteLine(ex);
                 MessageBox.Show($"Error adding playlist: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
             }
+        }
+
+        // Displays a status message and automatically hides the message after a 10-second delay using a timer.
+        private void ShowMessage()
+        {
+            StatusMessage = "Playlist has successfully been added";
+
+            // Timer to hide the message after a delay of 10 second
+            var timer = new DispatcherTimer { Interval = TimeSpan.FromSeconds(10) };
+            timer.Tick += (s, e) =>
+            {
+                StatusMessage = string.Empty;
+                timer.Stop();
+            };
+            timer.Start();
         }
     }
 }
--- MediaPlaylist/MediaPlaylist/ViewModels/PlaylistViewModels/UpdatePlaylistViewModel.cs
+++ MediaPlaylist/MediaPlaylist/ViewModels/PlaylistViewModels/UpdatePlaylistViewModel.cs
@@ -2,7 +2,6 @@
 using System.Diagnostics;
 using System.Windows;
 using System.Windows.Input;
-// 
 using MediaPlaylist.Core;
 using MediaPlaylist.Services;
 using MediaPlaylistBL;
@@ -12,15 +11,27 @@ namespace MediaPlaylist.ViewModels.PlaylistViewModels
 {
     public class UpdatePlaylistViewModel : ViewModelBase, INavigatable
     {
-        private readonly INavigationService _navigationService;
-        private readonly NavigationBarViewModel _navigationBarViewModel;
-        private readonly PlaylistManager _playlistManager;
+        private readonly ApplicationManager _appManager;
         private PlaylistViewModel _playlist;
 
 
         #region Properties
         public ObservableCollection<Media> Medias { get; private set; }
         public string HeaderTxt { get; private set; } = string.Empty;
+
+        public string _statusMessage = string.Empty;
+        public string StatusMessage
+        {
+            get => _statusMessage;
+            private set
+            {
+                if (_statusMessage != value)
+                {
+                    _statusMessage = value;
+                    OnPropertyChanged(nameof(StatusMessage));
+                }
+            }
+        }
 
         private string _title = string.Empty;
         public string Title
@@ -46,44 +57,45 @@ namespace MediaPlaylist.ViewModels.PlaylistViewModels
 
         #endregion
 
-        public ICommand UpdatePlaylistCommand { get; }
-        public ICommand RemoveMediaCommand { get; }
+        public AsyncCommandBase UpdatePlaylistCommand { get; }
+        public AsyncCommandBase RemoveMediaCommand { get; }
         public ICommand BackToPlaylistCommand { get; }
 
-        public UpdatePlaylistViewModel(INavigationService navigationService, PlaylistManager manager, NavigationBarViewModel navViewModel) 
-        { 
-            _navigationService = navigationService;
-            _playlistManager = manager;
-            _navigationBarViewModel = navViewModel;
+        public UpdatePlaylistViewModel(INavigationService navigationService, ApplicationManager manager)
+        {
+            _appManager = manager;
+            Medias = new ObservableCollection<Media>();
 
-            BackToPlaylistCommand = new CommandBase(_ => _navigationService.NavigateTo<PlaylistDetailsViewModel>(_playlist));
-            UpdatePlaylistCommand = new CommandBase(_ => UpdatePlaylist());
-            RemoveMediaCommand = new CommandBase(RemoveMedia);
+            BackToPlaylistCommand = new CommandBase(_ => navigationService.NavigateTo<PlaylistDetailsViewModel>(_playlist));
+            UpdatePlaylistCommand = new AsyncCommandBase(async _ => await UpdatePlaylist());
+            RemoveMediaCommand = new AsyncCommandBase(async (parameter) => await RemoveMedia(parameter));
         }
 
 
         public void Initialize(object parameter)
         {
+            StatusMessage = string.Empty;
+
             if (parameter is PlaylistViewModel playlistItem)
             {
                 _playlist = playlistItem;
                 HeaderTxt = $"Update {playlistItem.Title}";
                 Title = playlistItem.Title;
                 Description = playlistItem.Description;
-                Medias = new ObservableCollection<Media>(_playlist.Medias);
+                Medias.Clear();
+                _playlist.Medias.ForEach(m => Medias.Add(m));
             }
         }
 
-
-        private void RemoveMedia(object parameter)
+        private async Task RemoveMedia(object parameter)
         {
             try
             {
-               if (parameter is Media media)
-               {
-                    _playlistManager.RemoveMediaFromPlaylist(_playlist.Id, media.Id);
-                    Medias.Remove(media);  
-               }
+                if (parameter is Media media)
+                {
+                    await _appManager.RemoveMedia(media);
+                    Medias.Remove(media);
+                }
             }
             catch (Exception ex)
             {
@@ -92,36 +104,21 @@ namespace MediaPlaylist.ViewModels.PlaylistViewModels
             }
         }
 
-        private void UpdatePlaylist()
+        private async Task UpdatePlaylist()
         {
             try
             {
                 if (string.IsNullOrEmpty(Title)) throw new Exception("Title cannot be empty");
 
-                _playlistManager.UpdatePlaylist(_playlist.Id, Title, Description, _playlist.Medias);
-                ReplaceItem();
-                _navigationService.NavigateTo<PlaylistDetailsViewModel>(_playlist);
+                await _appManager.UpdatePlaylist(_playlist.Id, Title, Description);
+                Playlist getPlaylist = await _appManager.GetPlaylistById(_playlist.Id);
+                _playlist.RefreshData(getPlaylist);
+
+                StatusMessage = "Playlist has successfully been uppdated";
             }
-            catch (Exception ex) 
+            catch (Exception ex)
             {
-                //Debug.WriteLine(ex);
                 MessageBox.Show($"Error updating playlist: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-            }
-        }
-
-
-        /// <summary>
-        ///   Replaces an existing item in the playlists with a new item if it is found.
-        /// </summary>
-        private void ReplaceItem()
-        {
-            // Find the index of the item to replace
-            int index = _navigationBarViewModel.Playlists.IndexOf(_navigationBarViewModel.Playlists.FirstOrDefault(item => item.Id == _playlist.Id));
-
-            if (index != -1)
-            {
-                _navigationBarViewModel.Playlists[index] = null;
-                _navigationBarViewModel.Playlists[index] = _playlist;
             }
         }
     }
