a6f4c17eeff6643ecc3912e2a505a234f0ba0630
Ranim <ranim_hanna@hotmail.com>
2024-10-05 23:10:58 +0200

    Create Refresh Data method

--- MediaPlaylist/MediaPlaylist/ViewModels/PlaylistViewModels/PlaylistViewModel.cs
+++ MediaPlaylist/MediaPlaylist/ViewModels/PlaylistViewModels/PlaylistViewModel.cs
@@ -5,9 +5,10 @@ namespace MediaPlaylist.ViewModels.PlaylistViewModels
 {
     public class PlaylistViewModel : ViewModelBase
     {
-        private readonly Playlist _playlist;
+        private Playlist _playlist;
+        public Playlist Playlist => _playlist;
 
-        public int Id => _playlist.Id;
+        public int Id => _playlist.PlaylistId;
         public string Title => _playlist.Title;
         public string? Description => _playlist.Description;
         public string CreationDate => _playlist.CreationDate.ToString("g");
@@ -17,6 +18,22 @@ namespace MediaPlaylist.ViewModels.PlaylistViewModels
         public PlaylistViewModel(Playlist playlist)
         {
             _playlist = playlist;
+        }
+
+
+        // Refresh data manually when needed.
+        public void RefreshData(Playlist updatedPlaylist)
+        {
+            _playlist = updatedPlaylist;
+
+            // Notify all relevant properties
+            OnPropertyChanged(nameof(Playlist));
+            OnPropertyChanged(nameof(Id));
+            OnPropertyChanged(nameof(Title));
+            OnPropertyChanged(nameof(Description));
+            OnPropertyChanged(nameof(CreationDate));
+            OnPropertyChanged(nameof(LastModifiedDate));
+            OnPropertyChanged(nameof(Medias));
         }
     }
 }
