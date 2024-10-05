2416d92e093f9271f2d6907e008d6117b66b34bb
Ranim <ranim_hanna@hotmail.com>
2024-10-05 23:14:30 +0200

    Create LoadData method

--- MediaPlaylist/MediaPlaylist/ViewModels/NavigationBarViewModel.cs
+++ MediaPlaylist/MediaPlaylist/ViewModels/NavigationBarViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.ObjectModel;
+using System.Threading.Tasks;
 using System.Diagnostics;
 using System.Windows.Input;
 //
@@ -7,13 +8,15 @@ using MediaPlaylist.Core;
 using MediaPlaylist.Services;
 using MediaPlaylist.ViewModels.PlaylistViewModels;
 using MediaPlaylistBL;
+using MediaPlaylistStore;
+using System.Windows;
 
 namespace MediaPlaylist.ViewModels
 {
     public class NavigationBarViewModel : ViewModelBase
     {
         private readonly INavigationService _navigationService;
-        private PlaylistManager _playlistManager;
+        private readonly ApplicationManager _appManager;
 
         #region Properties
         public ObservableCollection<PlaylistViewModel> Playlists { get; set; }
@@ -55,17 +58,38 @@ namespace MediaPlaylist.ViewModels
         public ICommand NavigateToStartPageCommand { get; }
         public ICommand AddNewPlaylistCommand { get; }
 
-        public NavigationBarViewModel(INavigationService navigationService, PlaylistManager manager) 
+        public NavigationBarViewModel(INavigationService navigationService, ApplicationManager manager) 
         {
             _navigationService = navigationService;
-            _playlistManager = manager;
-            Playlists = new ObservableCollection<PlaylistViewModel>();
+            _appManager = manager;
+            LoadData();
 
             AddNewPlaylistCommand = new CommandBase(_ => NavigateToAddPlaylistPage());
             NavigateToStartPageCommand = new CommandBase(_ => NavigateToStartPage());
-
             //Playlists.CollectionChanged += OnPlaylistChanged;
         }
+
+
+        // Asynchronously loads playlist data from the data source and updates the UI.
+        private async void LoadData()
+        {
+            try
+            {
+                Playlists = new ObservableCollection<PlaylistViewModel>();
+                IEnumerable<Playlist> getPlaylists = await _appManager.GetPlaylists();
+
+                foreach (Playlist playlist in getPlaylists) 
+                {
+                    PlaylistViewModel viewModel = new PlaylistViewModel(playlist);
+                    Playlists.Add(viewModel);
+                }
+            }
+            catch (Exception ex) 
+            {
+                MessageBox.Show($"Error retrieving playlist: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
 
         private void NavigateToStartPage() 
         {
